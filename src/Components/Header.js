3aebc461cd58f62110ac47471b7ad48697c91ae9
daniel park <danielparkonline@gmail.com>
2019-12-17 18:20:28 -0800

    headshot/profilepic uploaded

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,9 +34,10 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h2 className="responsive-headline">Welcome to my domain!</h2>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">I'm {name}</h1>
+            {/* <h2 className="responsive-headline">Welcome to my domain!</h2> */}
+            <br></br>
+            <h3>{city} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
